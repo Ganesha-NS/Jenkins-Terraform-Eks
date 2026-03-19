279d13532f3108a9e85c623efa5e67300160a40f
GANESHA N S <ganeshns935@gmail.com>
2026-03-19 15:00:46 +0530

    Update eks.tf

--- terraform /eks.tf
+++ terraform /eks.tf
@@ -1,4 +1,3 @@
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
@@ -22,7 +21,6 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["m5.large"]
@@ -31,7 +29,7 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-      cluster-wg = {
+    cluster_wg = {
       min_size     = 1
       max_size     = 2
       desired_size = 1
